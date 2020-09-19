ead38be9627c5243e29c542af79cc094822fc0d9
Pritam Kute <pritam.kute>
2020-09-19 21:34:07 +0530

    React Hooks Birthday Countdown Timer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useRef, useState} from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [timerDays, setTimerDays] = useState('00');
+  const [timerHours, setTimerHours] = useState('00');
+  const [timerMinutes, setTimerMinutes] = useState('00');
+  const [timerSeconds, setTimerSeconds] = useState('00');
+
+  let interval = useRef();
+
+  const startTimer = () => {
+    const countdownDate = new Date('November 4, 2020 00:00:00').getTime();
+
+    interval = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = countdownDate - now;
+
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60)));
+      const minutes = Math.floor((distance % (1000 * 60 * 60) / (1000 * 60)));
+      const seconds = Math.floor((distance % (1000 * 60) / 1000));
+
+      if (distance < 0) {
+        //stop the timer
+        clearInterval(interval.current);
+      } else {
+        //update timer
+        setTimerDays(days);
+        setTimerHours(hours);
+        setTimerMinutes(minutes);
+        setTimerSeconds(seconds);
+      }
+    }, 1000);
+  };
+
+  useEffect(() => {
+    startTimer();
+    return () => {
+      clearInterval(interval.current);
+    };
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="timer-container">
+      <section className="timer">
+        <div>
+          <span className="mdi mdi-calendar-clock timer-icon"></span>
+          <h2>Bhagyashree's Birthday</h2>
+          <p>4th November, 2020</p>
+        </div>
+        <div>
+          <section>
+            <p>{timerDays}</p>
+            <p><small>Days</small></p>
+          </section>
+          <span>:</span>
+          <section>
+            <p>{timerHours}</p>
+            <p><small>Hours</small></p>
+          </section>
+          <span>:</span>
+          <section>
+            <p>{timerMinutes}</p>
+            <p><small>Minutes</small></p>
+          </section>
+          <span>:</span>
+          <section>
+            <p>{timerSeconds}</p>
+            <p><small>Seconds</small></p>
+          </section>
+        </div>
+      </section>
+    </section>
   );
-}
+};
 
 export default App;
